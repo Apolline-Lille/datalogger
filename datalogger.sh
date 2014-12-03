cea9afc6d9a5653f0d548b86e6bf7fc553d1524a
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2014-12-03 17:23:46 +0100

    launch datalogger: for either NDIR or WaspMote based on /dev/serial/by-id.

--- datalogger.sh
+++ datalogger.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-for dev in /dev/ttyUSB?
+#launch WaspMote (FTDI_FT232R_USB_UART_A9021)
+for dev in /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9021*
 do
-  echo 'start logging '$dev
+  echo 'start logging WaspMote from '$dev
+  dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
   nohup ./datalogger.py --device $dev &
+  sleep 1
+done
+
+#launch NDIR-CO2 (FTDI_FT232R_USB_UART_AM01V)
+for dev in /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AM01V*
+do
+  echo 'start logging NDIR-CO2 from'$dev
+  dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
+  nohup ./datalogger_CO2.py --device $dev &
   sleep 1
 done
 
@@ -11,4 +22,4 @@ done
 sleep 2
 
 #check
-ps aux | grep datalogger.py
+ps aux | grep datalogger --color
