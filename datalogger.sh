662e0328b7e9ca5d37aaef2f829d3414bbe16b6c
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2014-12-04 13:58:44 +0100

    launch all: add check if file exist.

--- datalogger.sh
+++ datalogger.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+#launch all datalogger for any devices reconized in /dev/serial/by-id/usb-*
+
 #launch WaspMote (FTDI_FT232R_USB_UART_A9021)
 for dev in /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9021*
 do
-  echo 'start logging WaspMote from '$dev
-  dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
-  nohup ./datalogger.py --device $dev &
-  sleep 1
+  if [ -e $dev ]
+  then
+    /bin/echo -n 'start logging WaspMote from '$dev
+    dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
+    echo ' i.e. '$dev
+    nohup ./datalogger.py --device $dev &
+    sleep 1
+  fi
 done
 
 #launch NDIR-CO2 (FTDI_FT232R_USB_UART_AM01V)
 for dev in /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AM01V*
 do
-  echo 'start logging NDIR-CO2 from'$dev
-  dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
-  nohup ./datalogger_CO2.py --device $dev &
-  sleep 1
+  if [ -e $dev ]
+  then
+    /bin/echo -n 'start logging NDIR-CO2 from '$dev
+    dev=/dev/ttyUSB`ls -lah $dev | tail -c 2 | head -c 1`
+    echo ' i.e. '$dev
+    nohup ./datalogger_CO2.py --device $dev &
+    sleep 1
+  fi
 done
 
 #wait
