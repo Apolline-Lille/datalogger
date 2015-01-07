f86103a93eefddde5f68f1598aaf57b23f59dbab
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2015-01-07 12:14:00 +0100

    startup: setup Arduino key world.

--- datalogger.sh
+++ datalogger.sh
@@ -4,6 +4,16 @@ for devbyid in /dev/serial/by-id/usb-*
 do
   dev=`ls -lah $devbyid | tail -c 8` #e.g. ttyUSB0 or ttyACM0
   dev=/dev/$dev
+  if((`echo $devbyid | grep Arduino | wc -l`>0)) #Arduino #UNO
+  then
+    echo $devbyid
+    echo $dev" is Arduino #UNO"
+  fi
+
+#  Arduino #UNO
+#  Prolific #DYLOS
+#  AM01 #AlphaSense CO2
+
   echo 'start logging '$dev'.'
 #  nohup ./datalogger.py --device $dev &
   sleep 1
