a69f8528162a820c4cfb3d35cb970461afcf88f5
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2015-01-19 10:00:01 +0100

    add OS device name script.

---
+++ datalogger_dev.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+version='v0.0.0'
+
+import string
+import os
+import argparse
+
+##path and file name
+#def get_file_name_base(module_name,current_time):
+#  return time.strftime('%Y/%m/',current_time)+module_name+time.strftime('%Y_%m_%d',current_time)
+
+
+os.system("ls")
