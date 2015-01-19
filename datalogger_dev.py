1ae6536722796a447af3bbb14697c51bcaea4992
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2015-01-19 11:10:49 +0100

    device name: setup code as a function.

--- datalogger_dev.py
+++ datalogger_dev.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python
 
-version='v0.0.1'
+version='v0.0.2'
 
 import string
 import subprocess
 
-##path and file name
-#def get_file_name_base(module_name,current_time):
-#  return time.strftime('%Y/%m/',current_time)+module_name+time.strftime('%Y_%m_%d',current_time)
+#path and device name
+def get_serial_pathNdevice_names():
+  #list serial devices by id (e.g. POSIX OS)
+  list=subprocess.check_output(["ls","-lah","/dev/serial/by-id"])
+#  print list
 
+  #make list as a list
+  list=list.splitlines()
+#  print list
 
-list=subprocess.check_output(["ls","-lah","/dev/serial/by-id"])
-#print list
+  #get device and name
+  device_name=[]
+  device_path=[]
+  for i in range(3,len(list)):
+  # print list[i]
+    tmp=list[i].split(" ")
+    device_name.append(tmp[9])
+    device_path.append(tmp[11])
 
-list=list.splitlines()
-#print list
+  #pack device name
+  for i in range(0,len(device_name)):
+    #clean name
+    device_name[i]=device_name[i].strip('usb-ifport0')
+    device_path[i]='/dev/'+device_path[i].strip('../../')
+    #specific name cleaning
+    ##Dylos
+    template='Prolific_Technology_Inc._USB-Serial_Controller_D'
+    if(device_name[i].rfind(template)==0):
+      device_name[i]='DYLOS'
+    ##FTDI_FT232R: WaspMote, AlphaSenseCO2, MX3cK/ADC
+    template='FTDI_FT232R_USB_UART_'
+    if(device_name[i].rfind(template)==0):
+      device_name[i]=device_name[i].replace(template,'')
+  #  print device_name[i], device_path[i]
 
-device_name=[]
-device_path=[]
-for i in range(3,len(list)):
-  #print list[i]
-  tmp=list[i].split(" ")
-  device_name.append(tmp[9])
-  device_path.append(tmp[11])
+  return device_path,device_name
 
-for i in range(0,len(device_name)):
-  device_name[i]=device_name[i].strip('usb-ifport0')
-  device_path[i]='/dev/'+device_path[i].strip('../../')
-  #Dylos
-  template='Prolific_Technology_Inc._USB-Serial_Controller_D'
-  if(device_name[i].rfind(template)==0):
-    device_name[i]='DYLOS'
-  #FTDI_FT232R: WaspMote, AlphaSenseCO2, MX3cK/ADC
-  template='FTDI_FT232R_USB_UART_'
-  if(device_name[i].rfind(template)==0):
-    device_name[i]=device_name[i].replace(template,'')
-  print device_name[i], device_path[i]
-
+#script call
+if __name__ == "__main__":
+  device_path,device_name=get_serial_pathNdevice_names()
+  for i in range(0,len(device_name)):
+    print device_name[i], device_path[i]
