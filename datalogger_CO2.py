a6adb135428547256fbf4e063256d62de794273e
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2015-01-19 12:23:53 +0100

    xsensor_path: setup module and call it for Xsensor/CO2

--- datalogger_CO2.py
+++ datalogger_CO2.py
@@ -8,7 +8,10 @@ import os
 import time
 import argparse
 
+#path and device name
 import xsensor_device
+#path and file name
+import xsensor_path
 
 ##set column names
 #colnames(t)=c("time","index","temperature","CO2","lamp")
@@ -18,25 +21,6 @@ import xsensor_device
 #T	26.6
 #V	 400
 
-#path and file name
-def get_path_name_base(current_time):
-  path=time.strftime('%Y/%m/',current_time)
-  #path exist
-  if(not os.access(path,os.F_OK)):
-    os.makedirs(path)
-  return path
-
-def get_file_name_base(module_name,current_time):
-  return get_path_name_base(current_time)+module_name+time.strftime('%Y_%m_%d',current_time)
-
-def get_data_file_name(module_name,current_time):
-  return get_file_name_base(module_name+'_',current_time)+'.txt'
-
-def get_raw_file_name(module_name,current_time):
-  return get_file_name_base(module_name+'_',current_time)+'.raw'
-
-def get_info_file_name(current_time,hostname):
-  return get_file_name_base(hostname+'_',current_time)+'.info'
 
 #CLI arguments
 serialDev='/dev/ttyUSB0'
@@ -44,9 +28,9 @@ fake='test.raw'
 module='NDIRwCO2'+'_'+xsensor_device.get_serial_device_name(serialDev)
 hostname='raspc2aN'
 current_time=time.localtime()
-info_file_name=get_info_file_name(current_time,hostname)
-file_name=get_data_file_name(module,current_time)
-raw_file_name=get_raw_file_name(module,current_time)
+info_file_name=xsensor_path.get_info_file_name(current_time,hostname)
+file_name=xsensor_path.get_data_file_name(module,current_time)
+raw_file_name=xsensor_path.get_raw_file_name(module,current_time)
 parser = argparse.ArgumentParser(
   description='log data from NDIR/CO2 sensor.'
   +' At least, 3 files will be written:'
@@ -87,7 +71,7 @@ print str_time
 #set info file name from date
 import platform
 hostname=platform.node()
-info_file_name=get_info_file_name(current_time,hostname)
+info_file_name=xsensor_path.get_info_file_name(current_time,hostname)
 
 #write to information file
 fi=open(info_file_name,"a")
@@ -99,8 +83,8 @@ fi.write(module)
 fi.close() #information file
 
 #set both raw and data file names from both module and date
-file_name=get_data_file_name(module,current_time)
-raw_file_name=get_raw_file_name(module,current_time)
+file_name=xsensor_path.get_data_file_name(module,current_time)
+raw_file_name=xsensor_path.get_raw_file_name(module,current_time)
 
 iteration=0
 
@@ -139,8 +123,8 @@ while(True): #loop on both sensors and time
   line+="\n"
   print line
   #setup file name from date
-  file_name=get_data_file_name(module,current_time)
-  raw_file_name=get_raw_file_name(module,current_time)
+  file_name=xsensor_path.get_data_file_name(module,current_time)
+  raw_file_name=xsensor_path.get_raw_file_name(module,current_time)
   #write to file
   fo=open(file_name,"a")
   fo.write(line)
---
+++ xsensor_path.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+version='v0.0.0'
+
+import string
+import os
+import time
+import argparse
+
+#path and file name
+def get_path_name_base(current_time):
+  path=time.strftime('%Y/%m/',current_time)
+  #path exist
+  if(not os.access(path,os.F_OK)):
+    os.makedirs(path)
+  return path
+
+def get_file_name_base(module_name,current_time):
+  return get_path_name_base(current_time)+module_name+time.strftime('%Y_%m_%d',current_time)
+
+def get_data_file_name(module_name,current_time):
+  return get_file_name_base(module_name+'_',current_time)+'.txt'
+
+def get_raw_file_name(module_name,current_time):
+  return get_file_name_base(module_name+'_',current_time)+'.raw'
+
+def get_info_file_name(current_time,hostname):
+  return get_file_name_base(hostname+'_',current_time)+'.info'
